d645e9f7cbafc7cb42e124ca5f10adf73f4bb080
ashiqurras <ashiqurrussel2@gmail.com>
2024-11-21 19:33:36 +0100

    Add server setup

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
