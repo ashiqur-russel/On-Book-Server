b245bb6d5ae377b06a01a593dc7ae76dcf3ac3ea
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-01-26 20:31:00 +0100

    Add generic response and refactor controllers

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,146 +1,78 @@
 import { Request, Response } from 'express';
 import productService from './product.service';
+import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
+import httpStatus from 'http-status';
 
 // Retirve all the products from database created
-const getAllProducts = async (req: Request, res: Response) => {
+const getAllProducts = catchAsync(async (req: Request, res: Response) => {
   const searchTerm = req.query.searchTerm as string;
-  try {
-    const products = await productService.getAllProducts(searchTerm);
-    res.status(200).json({
-      message: 'Books retrieved successfully',
-      status: true,
-      data: products,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Validation failed', success: false, error });
-  }
-};
+  const products = await productService.getAllProducts(searchTerm);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Books retrieved successfully',
+    data: products,
+  });
+});
 
 // Fetch product by id
-const getProductById = async (req: Request, res: Response) => {
+const getProductById = catchAsync(async (req: Request, res: Response) => {
   const { productId } = req.params;
+  const product = await productService.getProductById(productId);
 
-  try {
-    const product = await productService.getProductById(productId);
-
-    res.status(200).json({
-      message: 'Book retrieved successfully',
-      success: true,
-      data: product,
-    });
-  } catch (error) {
-    const err = error as Error;
-    if (err.name !== 'ValidationError') {
-      res.status(404).json({
-        message: err.message,
-        success: false,
-      });
-    } else {
-      res.status(500).json({
-        message: err.message,
-        status: false,
-        err,
-        stack: err.stack,
-      });
-    }
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Book retrieved successfully',
+    data: product,
+  });
+});
 
 // Create product
-const createProduct = async (req: Request, res: Response) => {
-  try {
-    const product = await productService.createProduct(req.body);
+const createProduct = catchAsync(async (req: Request, res: Response) => {
+  const product = await productService.createProduct(req.body);
 
-    res.status(201).json({
-      message: 'Book created successfully',
-      success: true,
-      data: product,
-    });
-  } catch (error) {
-    const err = error as Error;
-    if (err.name !== 'ValidationError') {
-      res.status(500).json({
-        message: err.message,
-        success: false,
-      });
-    } else {
-      res.status(400).json({
-        message: 'Validation failed',
-        success: false,
-        error,
-        stack: err.stack,
-      });
-    }
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Book created successfully',
+    data: product,
+  });
+});
 
 // update product data; partial data also could be updated
-const updateProduct = async (req: Request, res: Response) => {
+const updateProduct = catchAsync(async (req: Request, res: Response) => {
   const { productId } = req.params;
   const updateData = req.body;
 
-  try {
-    const updatedProduct = await productService.updateProduct(
-      productId,
-      updateData,
-    );
+  const updatedProduct = await productService.updateProduct(
+    productId,
+    updateData,
+  );
 
-    res.status(201).json({
-      message: 'Book updated successfully',
-      success: true,
-      data: updatedProduct,
-    });
-  } catch (error) {
-    const err = error as Error;
-    if (err.name !== 'ValidationError') {
-      res.status(404).json({
-        message: err.message,
-        success: false,
-      });
-    } else {
-      res.status(500).json({
-        message: 'Validation failed',
-        success: false,
-        error,
-        stack: err.stack,
-      });
-    }
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Book updated successfully',
+    data: updatedProduct,
+  });
+});
 
 // Delete product using product id
-const deleteProduct = async (req: Request, res: Response) => {
+const deleteProduct = catchAsync(async (req: Request, res: Response) => {
   const { productId } = req.params;
 
-  try {
-    await productService.deleteProduct(productId);
+  await productService.deleteProduct(productId);
 
-    res.status(201).json({
-      message: 'Book deleted successfully',
-      success: true,
-      data: {},
-    });
-  } catch (error) {
-    const err = error as Error;
-    if (err.name !== 'ValidationError') {
-      res.status(404).json({
-        message: err.message,
-        success: false,
-        err,
-        stack: err.stack,
-      });
-    } else {
-      res.status(500).json({
-        message: err.message,
-        status: false,
-        err,
-        stack: err.stack,
-      });
-    }
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Book deleted successfully',
+    data: {},
+  });
+});
 
 const productController = {
   updateProduct,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,35 +1,21 @@
-import { Request, Response } from 'express';
 import { IUser } from './user.interface';
 import { UserServices } from './user.service';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
-    const { user: userData } = req.body;
+const registerUser = catchAsync(async (req, res) => {
+  const userData: IUser = req.body;
 
-    const user = await UserServices.createUser(userData as IUser);
+  const result = await UserServices.createUser(userData);
 
-    res.status(201).json({
-      message: 'User created successfully',
-      success: true,
-      data: user,
-    });
-  } catch (error) {
-    const err = error as Error;
-    if (err.name !== 'ValidationError') {
-      res.status(500).json({
-        message: err.message,
-        success: false,
-      });
-    } else {
-      res.status(400).json({
-        message: 'Validation failed',
-        success: false,
-        error,
-        stack: err.stack,
-      });
-    }
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created succesfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   registerUser,
